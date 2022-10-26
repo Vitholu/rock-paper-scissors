aa18111530c35c296879504d29fcd9f76cfd0c4e
Vitholu <vitholumtech@gmail.com>
2022-10-26 19:21:34 +0200

    Add RPS logic and win/lose messages

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ function computerChoice() {
 	}
 	return choice;
 }
+
+function rpsWinner(user, computer) {
+	user = userChoice('Rock');
+	computer = computerChoice();
+	if (user === 'Rock' && computer != 'Paper') {
+		if (user === computer) {
+			return alert('Draw');
+		}
+		return alert('You Won! ' + 'Computer choice was: ' + computer);
+	}
+	if (user === 'Paper' && computer != 'Scissor') {
+		if (user === computer) {
+			return alert('Draw');
+		}
+		return alert('You Won! ' + 'Computer choice was: ' + computer);
+	}
+	if (user === 'Scissor' && computer != 'Rock') {
+		if (user === computer) {
+			return alert('Draw');
+		}
+		return alert('You Won! ' + 'Computer choice was: ' + computer);
+	} else {
+		return alert('You Lose! ' + 'Computer choice was: ' + computer);
+	}
+
+	return console.log('Thanks for playing');
+}
