febf3cc33e4858335fc48d173658f14b3bb555c7
Vitholu <vitholumtech@gmail.com>
2022-10-26 19:02:46 +0200

    Add computer choice logic

--- script.js
+++ script.js
@@ -4,5 +4,17 @@ function userChoice(choice) {
 
 function computerChoice() {
 	let randomNumber = Math.floor(Math.random() * 3) + 1;
-	return randomNumber;
+	let choice;
+	if (randomNumber != '') {
+		if (randomNumber === 1) {
+			return (choice = 'Rock');
+		}
+		if (randomNumber === 2) {
+			return (choice = 'Paper');
+		}
+		if (randomNumber === 3) {
+			return (choice = 'Scissor');
+		}
+	}
+	return choice;
 }
